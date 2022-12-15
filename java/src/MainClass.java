98ac356b731240ca1171f86c7a0714baf760495b
cargalovan <a.kargalova@indriver.com>
2022-12-15 21:55:01 +0600

    Первый урок, тест 1

---
+++ java/src/MainClass.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+public class MainClass extends MainClassTest {
+    int a = 14;
+
+    @Test
+    public void myFirstTest() {
+        int a = this.multiply(14);
+        System.out.println(a);
+
+    }
+
+}
