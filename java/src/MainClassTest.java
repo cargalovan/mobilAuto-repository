6b5f725bd60a326b872a87c5705d98cc08654c9f
cargalovan <a.kargalova@indriver.com>
2022-12-15 23:37:06 +0600

    Первый урок, тест1

---
+++ java/src/MainClassTest.java
@@ -0,0 +1,10 @@
+public class MainClassTest {
+    public int multiply(int number) {
+        return number * 1;
+    }
+
+    public int multiply(int number, int multiply) {
+        return number * multiply;
+    }
+
+}
